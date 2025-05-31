78b2fafc018273a6b659ceafd39935a3c02a55fa
8ctopotamus <zylo.codes@gmail.com>
2025-05-30 20:23:02 -0500

    counter component

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use leptos::prelude::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+    
+    view! {
+        <button on:click=move |_| *set_count.write() += 1>
+            "Click me:" {count}
+        </button>
+        <p>
+            "Frozen count: " 
+            {count.get()}
+        </p>
+        <p>
+            "Double count: " 
+            {move || count.get() * 2}
+        </p>
+    }
+}
+
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>Hello world!</p> })
+    leptos::mount::mount_to_body(|| view! { <App /> })
 }
