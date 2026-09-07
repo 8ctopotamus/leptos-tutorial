b19bef05b56cb9750898b0ef2a37c4ae9ace9564
Josh <zylo.codes@gmail.com>
2026-09-07 09:23:07 -0500

    Iterating over complex data, reactivity with signals

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,38 @@ use leptos::prelude::*;
 #[derive(Debug, Clone)]
 struct DatabaseEntry {
     key: String,
-    value: i32
+    value: RwSignal<i32>
 }
 
 #[component]
 fn App() -> impl IntoView {
-    let (data, set_data) = signal(vec![
+    let (data, _set_data) = signal(vec![
         DatabaseEntry {
             key: "foo".to_string(),
-            value: 10,
+            value: RwSignal::new(10),
         },
         DatabaseEntry {
             key: "bar".to_string(),
-            value: 20,
+            value: RwSignal::new(20),
         },
         DatabaseEntry {
             key: "baz".to_string(),
-            value: 15,
+            value: RwSignal::new(15),
         },
     ]);
 
     view! {
         <button on:click=move |_| {
-            set_data.update(|data| {
-                for row in data {
-                    row.value *= 2;
-                }
-            });
+            for row in &*data.read() {
+                row.value.update(|value| *value *= 2);
+            }
             leptos::logging::log!("{:?}", data.get());
         }>
             "Update values"
         </button>
         <For
             each=move || data.get()
-            key=|state| (state.key.clone(), state.value.clone())
+            key=|state| state.key.clone()
             let(child)
         >
            <p>{child.value}</p>        
