6e4fa27eac5fb0f610813474d23e84b5979507f5
8ctopotamus <zylo.codes@gmail.com>
2026-09-11 09:00:23 -0500

    Controlled inputs

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,18 @@
 use leptos::prelude::*;
-use reactive_stores::{Store, StoreFieldIterator};
-
-#[derive(Store, Debug, Clone)]
-pub struct DataStore {
-    #[store(key: String = |row| row.key.clone())]
-    rows: Vec<DatabaseEntry>,
-}
-
-
-#[derive(Store, Debug, Clone)]
-struct DatabaseEntry {
-    key: String,
-    value: i32
-}
 
 #[component]
 fn App() -> impl IntoView {
-    let data = Store::new(DataStore { 
-        rows: vec![
-            DatabaseEntry {
-                key: "foo".to_string(),
-                value: 10,
-            },
-            DatabaseEntry {
-                key: "bar".to_string(),
-                value: 20,
-            },
-            DatabaseEntry {
-                key: "baz".to_string(),
-                value: 15,
-            },
-        ],
-    });
+    let (name, set_name) = signal("Controlled".to_string());
 
     view! {
-        <button on:click=move |_| {
-            for row in data.rows().iter_unkeyed() {
-                *row.value().write() *= 2;
+        <input 
+            prop:value=name
+            on:input:target=move |e| {
+                set_name.set(e.target().value());
             }
-            leptos::logging::log!("{:?}", data.get());
-        }>
-            "Update values"
-        </button>
-        <For
-            each=move || data.rows()
-            key=|row| row.read().key.clone()
-            children=move |child| {
-                let value = child.value();
-                view! {
-                    <p>{move || value.get()}</p>
-                }
-            }
+            type="text"
         />
+        <p>"Name is: " {name}</p> 
     }
 }
 
