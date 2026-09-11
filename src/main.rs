73ce84b9505decc44c8676a32044a33bb0d37a51
8ctopotamus <zylo.codes@gmail.com>
2026-09-11 08:50:17 -0500

    using stores

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use leptos::prelude::*;
+use reactive_stores::{Store, StoreFieldIterator};
 
-#[derive(Debug, Clone)]
+#[derive(Store, Debug, Clone)]
+pub struct DataStore {
+    #[store(key: String = |row| row.key.clone())]
+    rows: Vec<DatabaseEntry>,
+}
+
+
+#[derive(Store, Debug, Clone)]
 struct DatabaseEntry {
     key: String,
     value: i32
@@ -8,40 +16,39 @@ struct DatabaseEntry {
 
 #[component]
 fn App() -> impl IntoView {
-    let (data, set_data) = signal(vec![
-        DatabaseEntry {
-            key: "foo".to_string(),
-            value: 10,
-        },
-        DatabaseEntry {
-            key: "bar".to_string(),
-            value: 20,
-        },
-        DatabaseEntry {
-            key: "baz".to_string(),
-            value: 15,
-        },
-    ]);
+    let data = Store::new(DataStore { 
+        rows: vec![
+            DatabaseEntry {
+                key: "foo".to_string(),
+                value: 10,
+            },
+            DatabaseEntry {
+                key: "bar".to_string(),
+                value: 20,
+            },
+            DatabaseEntry {
+                key: "baz".to_string(),
+                value: 15,
+            },
+        ],
+    });
 
     view! {
         <button on:click=move |_| {
-            set_data.update(|items| {
-                for item in items.iter_mut() {
-                    item.value *= 2;
-                }
-            });
+            for row in data.rows().iter_unkeyed() {
+                *row.value().write() *= 2;
+            }
+            leptos::logging::log!("{:?}", data.get());
         }>
             "Update values"
         </button>
-        <ForEnumerate
-            each=move || data.get()
-            key=|state| state.key.clone()
-            children=move |index, _| {
-                let value = Memo::new(move |_| {
-                    data.with(|data| data.get(index.get()).map(|d| d.value).unwrap_or(0))
-                });
+        <For
+            each=move || data.rows()
+            key=|row| row.read().key.clone()
+            children=move |child| {
+                let value = child.value();
                 view! {
-                    <p>{value}</p>
+                    <p>{move || value.get()}</p>
                 }
             }
         />
