a677ceabdd52d51b32c81e9ac60ccfe9c4f172cf
Josh <zylo.codes@gmail.com>
2026-09-13 09:37:01 -0500

    basic control flow

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,17 @@ use leptos::prelude::*;
 
 #[component]
 fn App() -> impl IntoView {
-    let (some_textarea_value, set_some_textarea_value) = signal("".to_string());
+    let (value, set_value) = signal(0);
+    let is_odd = move || value.get() % 2 != 0;
 
-    let (select_value, set_select_value) = signal(0i32);
-    
     view! {
-        <textarea
-            prop:value=move || some_textarea_value.get()
-            on:input:target=move |e| set_some_textarea_value.set(e.target().value())
-        >
-            {some_textarea_value}
-        </textarea>
-        <p><strong>"Message: "</strong> {some_textarea_value}</p>
-        <select
-            prop:value=select_value
-            on:change:target=move |e| {
-                set_select_value.set(e.target().value().parse().unwrap());
-            } 
-        >
-            <option value="0">"0"</option>
-            <option value="1">"1"</option>
-            <option value="2">"2"</option>
-        </select>
-        <button on:click=move |_| set_select_value.update(|n| {
-            if *n == 2 {
-                *n = 0;
+        <p>
+            {move || if is_odd() {
+                "odd"
             } else {
-                *n += 1;
-            }
-        })>
-            "Next option"
-        </button>
-        <p><strong>"Select value: "</strong> {select_value}</p>
+                "even"
+            }}
+        </p>
     }
 }
 
