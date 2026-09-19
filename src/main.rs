5d29e31dfcdd7c134a04dbba2a00a559377187a7
Josh <zylo.codes@gmail.com>
2026-09-19 08:22:32 -0500

    Working with signals - 1

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use leptos::prelude::*;
-use leptos::html::p;
 
 #[component]
 fn App() -> impl IntoView {
-    let (value, _set_value) = signal(0);
+    let (names, set_names) = signal(Vec::new());
 
-    // use component props builder
-    // Show(
-    //     ShowProps::builder()
-    //         .when(move || value.get() > 5)
-    //         .fallback(|| p().child("I will appear if `value` is 5 or lower"))
-    //         .children(ToChildren::to_children(|| {
-    //             p().child("I will appear if `value is above 5")
-    //         }))
-    //         .build(),
-    // )
+    // this code is inefficient because we are cloning the whole Vec<string>
+    // just to check .is_empty(), and then throwing away the clone
+    // let _ = if names.get().is_empty() {
+        // this .set is also inefficient because we are replacing the value with a whole new Vec<String>...
+        // set_names.set(vec!["Alice".to_string()]);
+    // };
 
-    // or directly build the props struct
-    Show(ShowProps {
-        when: move || value.get() > 5,
-        fallback: (|| p().child("I will appear if `value` is 5 or lower")).into(),
-        children: ToChildren::to_children(|| p().child("I will appear if `value is above 5")),
-    })
+    // instead we can:
+
+    // use names by reference to avoid the clone using `.read()`
+    if names.read().is_empty() {
+        // mutate the original Vec<String> in place using `.write()`
+        set_names.write().push("Alice".to_string());
+    }
+
+    view!  {
+        <ul>
+            {names.get().into_iter()
+                .map(|n| view! { <li>{n}</li> })
+                .collect::<Vec<_>>()}
+        </ul>
+    }
 }
 
 fn main() {
