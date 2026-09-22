3ef3b9b6cd883ad39ce1f96600d30d38d4eda625
8ctopotamus <zylo.codes@gmail.com>
2026-09-22 08:52:29 -0500

    Loading data with Resources

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,49 @@
 use leptos::prelude::*;
-use leptos::logging::{log};
+use gloo_timers::future::TimeoutFuture;
+
+async fn load_data(value: i32) -> i32 {
+    TimeoutFuture::new(2_000).await;
+    value * 10
+}
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, _set_count) = signal(0);
+    let (count, set_count) = signal(0);
 
-    let _double_count = move || count.get() * 2;
-    let count_is_odd = move || count.get() & 1 == 1;
-    let text = move || if count_is_odd() {
-        "odd"
-    } else {
-        "even"
+    // a resource will only load once if it doesn't read any reactive data
+    let stable = LocalResource::new(|| load_data(1));
+
+    // tracks count and calls load_data whenever it changes
+    let async_data = LocalResource::new(move || load_data(count.get()));
+
+    // we can access the resource values with .get()
+    // this will reactively return None before the Future has resolved
+    // and update to Some(T) when it has resolved
+    let async_result = move || {
+        async_data
+            .get()
+            .map(|value| format!("Server returned {value:?}"))
+            .unwrap_or_else(|| "Loading".into())
     };
 
-    // effects automatically track the signals it depends
-    // and re-reuns when they change
-    Effect::new(move |_| {
-        log!("text = {}", text());
-    });
-
     view! {
-        <p>{move || text().to_uppercase()}</p>
+        <button
+            on:click=move |_| *set_count.write() += 1
+        >
+            "Click me"
+        </button>
+        <p>
+            <code>"stable"</code>": " {move || stable.get()}
+        </p>
+        <p>
+            <code>"count"</code>": " {count}
+        </p>
+        <p>
+            <code>"async_value"</code>": "
+            {async_result}
+            <br/>
+        </p>
+
     }
 }
 
