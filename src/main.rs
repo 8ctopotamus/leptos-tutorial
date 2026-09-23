e9790112e2340a0b39b2f2c9381d6be824e1e6dc
Josh <zylo.codes@gmail.com>
2026-09-23 16:01:33 -0500

    Global state mgmt: Passing signals through context

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,33 @@
-use leptos::{html::Input, prelude::*};
-use gloo_timers::future::TimeoutFuture;
-use uuid::Uuid;
-
-async fn add_todo(text: &str) -> Uuid {
-    _ = text;
-    // fake a one-second delay
-    // SendWrapper allows us to use this !Send browser API; don't worry about it
-    send_wrapper::SendWrapper::new(TimeoutFuture::new(1_000)).await;
-    // pretend this is a post ID or something
-    Uuid::new_v4()
-}
+use leptos::prelude::*;
 
 #[component]
 fn App() -> impl IntoView {
-    // an action takes an async function with single argument
-    // it can be a simple type, a struct, or ()
-    let add_todo = Action::new(|input: &String| {
-        // the input is a reference, but we need the Future to own it
-        // this is important: we need to clone and move into the Future
-        // so it has a 'static lifetime
-        let input = input.to_owned();
-        async move { add_todo(&input).await }
-    });
+    let (count, _set_count) = signal::<u32>(0);
 
-    // actions provide a bunch of synchronous, reactive variables
-    // that tell us different things about the state of the action
-    let submitted = add_todo.input();
-    let pending = add_todo.pending();
-    let todo_id = add_todo.value();
-
-    let input_ref = NodeRef::<Input>::new();
+    provide_context(count);
 
     view! {
-        <form on:submit=move |ev| {
-            ev.prevent_default();
-            let input = input_ref.get().expect("input to exist");
-            add_todo.dispatch(input.value());
-        }>
-            <label>
-                "What do you need to do?"
-                <input 
-                    node_ref=input_ref
-                    type="text" 
-                />
-            </label>
-            <button type="submit">"Add Todo"</button>
-        </form>
-        <p>{move || pending.get().then_some("Loading...")}</p>
-        <p>
-            "Submitted: "
-            <code>{move || format!("{:#?}", submitted.get())}</code>
-        </p>
-        <p>
-            "Pending: "
-            <code>{move || format!("{:#?}", pending.get())}</code>
-        </p>
-        <p>
-            "Todo ID: "
-            <code>{move || format!("{:#?}", todo_id.get())}</code>
-        </p>
+        <FancyMath />
+    }
+}
+
+#[component]
+fn FancyMath() -> impl IntoView {
+    let count = use_context::<ReadSignal<u32>>()
+        .expect("there to be a `count` signal provided");
+    let is_even = move || count.get() & 1 == 0;
+
+    view! {
+        <div>
+            "The number " 
+            <strong>{count}</strong>
+            {move || if is_even() {
+                " is"
+            } else {
+                " is not"
+            }}
+            " even."
+        </div>
     }
 }
 
